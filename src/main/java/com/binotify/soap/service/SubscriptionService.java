652a83758364ba526b7f4c62fe58768565e8968a
Fachry Dennis Heraldi <3688+dennisheraldi@users.noreply.gitlab.informatika.org>
2022-12-01 16:26:34 +0700

    feat: subscription request

--- src/main/java/com/binotify/soap/service/SubscriptionService.java
+++ src/main/java/com/binotify/soap/service/SubscriptionService.java
@@ -45,6 +45,29 @@ public class SubscriptionService {
         throw new SQLException("Not authenticated");
     }
 
+    public int ApprovalSub(@WebParam(name="CreatorId") int creator_id, @WebParam(name="SubscriberId") int subscriber_id, @WebParam(name="IsApproved") boolean isApproved) throws SQLException {
+        Logger.log(context, 
+            "[REST] Changing PENDING approval subscription to " + (isApproved ? "ACCEPTED" : "REJECTED") +
+            "from subscriber id " + subscriber_id + " for creator id " + creator_id,
+            "/binotify/Subscription{ApprovalSub}"
+        );
+        if (Authentication.IsAuthenticated(context, ServiceType.REST)) {
+            Subscription s = Subscription.get(creator_id, subscriber_id);
+            if (s != null) {
+                s.status = isApproved ? Subscription.Status.ACCEPTED : Subscription.Status.REJECTED;
+                s.save();
+                // Callback to the PHP service
+                Request.POST(BodyPublishers.ofString("result=" + (isApproved ? "ACCEPTED" : "REJECTED")),
+                    ServiceType.PHP, "/webhook/subscribe/" + creator_id + "/" + subscriber_id + "/",
+                    System.out::println
+                );
+                return 1; // Success changing status
+            }
+            return 0; // Subscription not found
+        }
+        throw new SQLException("Not authenticated");
+    }
+
     public void TestMail(@WebParam(name="Destination") String dest, @WebParam(name="Subject") String subject, @WebParam(name="Message") String message) {
         Mailer.notifyAdminNewSub("New subscription from {sub_id} for {creat_id} just dropped!", "[{\"email\":\"" + dest + "\"}]");
     }
