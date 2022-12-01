1beb271abc05a8117d9e5ead413e949062b0bba5
Amar Fadil <34912365+marfgold1@users.noreply.github.com>
2022-12-01 16:13:10 +0700

    feat: add endpoint to test

--- src/main/java/com/binotify/soap/service/SubscriptionService.java
+++ src/main/java/com/binotify/soap/service/SubscriptionService.java
@@ -1,8 +1,24 @@
 package com.binotify.soap.service;
 
+import java.net.http.HttpRequest.BodyPublishers;
+
+import com.binotify.soap.enums.ServiceType;
+import com.binotify.soap.utils.*;
+
+import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 
 @WebService
 public class SubscriptionService {
+    public void TestMail(@WebParam(name="Destination") String dest, @WebParam(name="Subject") String subject, @WebParam(name="Message") String message) {
+        Mailer.notifyAdminNewSub("New subscription from {sub_id} for {creat_id} just dropped!", "[{\"email\":\"" + dest + "\"}]");
+    }
 
+    public void TestHook(@WebParam(name="CreatorId") int creator_id, @WebParam(name="SubscriberId") int subscriber_id, @WebParam(name="IsApproved") boolean isApproved) {
+        // Callback to the PHP service
+        Request.POST(BodyPublishers.ofString("result=" + (isApproved ? "ACCEPTED" : "REJECTED")),
+            ServiceType.PHP, "/webhook/subscribe/" + creator_id + "/" + subscriber_id + "/",
+            System.out::println
+        );
+    }
 }
